dc1a8f675638b65815db504c969f2f10b053e275
Biraj KC <biraaj.kc@gmail.com>
2025-04-10 13:06:02 +1000

    Enhance ML pipeline with model evaluation metrics and persistence functionality

--- scripts/ml_pipeline.py
+++ scripts/ml_pipeline.py
@@ -1,3 +1,5 @@
+# This scripts performs the following tasks: ML Pipeline Creation, Evaluation of Model, and Model Persistence using PySpark.
+
 # Part 2 : Machine Learning Pipeline Creation
 
 # Importing required libraries
@@ -62,3 +64,69 @@ ps = [str(x).split("_")[0] for x in pipeline.getStages()]
 print("Pipeline Stage 1 =", ps[0])
 print("Pipeline Stage 2 =", ps[1])
 print("Pipeline Stage 3 =", ps[2])
+
+
+# Part 3 - Model Evaluation
+# Task 1 - Predict using the model
+predictions = pipelineModel.transform(testingData)
+
+# Task 2 - Print the MSE (Mean Squared Error)
+from pyspark.ml.evaluation import RegressionEvaluator
+
+evaluator = RegressionEvaluator(
+    predictionCol="prediction", labelCol="MPG", metricName="mse"
+)
+mse = evaluator.evaluate(predictions)
+print(mse)
+
+# Task 3 - Print the MAE (Mean Absolute Error)
+evaluator = RegressionEvaluator(
+    predictionCol="prediction", labelCol="MPG", metricName="mae"
+)
+mae = evaluator.evaluate(predictions)
+print(mae)
+
+# Task 4 - Print the R-squared (R2)
+evaluator = RegressionEvaluator(
+    predictionCol="prediction", labelCol="MPG", metricName="r2"
+)
+r2 = evaluator.evaluate(predictions)
+print(r2)
+
+# Part 3 - Evaluation
+print("Part 3 - Evaluation")
+
+print("Mean Squared Error =", round(mse, 2))
+print("Mean Absolute Error =", round(mae, 2))
+print("R squared (R2) =", round(r2, 2))
+
+lrModel = pipelineModel.stages[-1]
+
+print("Intercept =", round(lrModel.intercept, 2))
+
+
+# Part 4 - Model Persistence
+# Task 1 - Save the model to the path "Practice Project"
+
+pipelineModel.write().overwrite().save("Practice_Project")
+# Task 2 - Load the model from the path "Practice_Project"
+
+loadedPipelineModel = PipelineModel.load("Practice_Project")
+# Task 3 - Make Predictions using the loaded model on the test data
+
+predictions = loadedPipelineModel.transform(testingData)
+# Task 4 - Show the predictions
+
+predictions.select("MPG", "prediction").show()
+# Part 4 - Evaluation
+
+loadedmodel = loadedPipelineModel.stages[-1]
+totalstages = len(loadedPipelineModel.stages)
+inputcolumns = loadedPipelineModel.stages[1].getInputCols()
+
+print("Number of stages in the pipeline =", totalstages)
+for i, j in zip(inputcolumns, loadedmodel.coefficients):
+    print(f"Coefficient for {i} is {round(j, 4)}")
+# Task 5 - Stopping Spark
+
+spark.stop()
